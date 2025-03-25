97fe37c128f32d53a1c3f6963a1a851f870cb799
shakgill786 <sgillani1@gmail.com>
2025-03-25 11:28:48 -0400

    updated css and working login logout routes

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,52 @@
 import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
+import { useDispatch, useSelector } from "react-redux";
+import { thunkLogout } from "../../redux/session";
 import "./Navigation.css";
 
 function Navigation() {
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(thunkLogout());
+  };
+
+  const capitalize = (str) => {
+    if (!str) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <NavLink to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/96/UNESCO_logo.svg"
+            alt="Logo"
+            className="logo"
+          />
+        </NavLink>
+      </div>
+
+      <div className="navbar-title">Luxury Eats</div>
+
+      <div className="navbar-links">
+        <NavLink to="/">Home</NavLink>
+
+        {user ? (
+          <>
+            <span className="welcome-user">Welcome, {capitalize(user.username)}</span>
+            <button onClick={logout}>Log Out</button>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Log In</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </>
+        )}
+      </div>
+    </nav>
   );
 }
 
