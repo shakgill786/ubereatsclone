464e0fcfe756192bda16b3603ecf88380f871fb6
shakgill786 <sgillani1@gmail.com>
2025-04-01 11:44:34 -0400

    render login issue

--- app/__init__.py
+++ app/__init__.py
@@ -51,9 +51,10 @@ def inject_csrf_token(response):
         generate_csrf(),
         secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
         samesite='Strict' if os.environ.get('FLASK_ENV') == 'production' else None,
-        httponly=True
+        httponly=False  
     )
     return response
+
 
 @app.route("/api/docs")
 def api_help():
