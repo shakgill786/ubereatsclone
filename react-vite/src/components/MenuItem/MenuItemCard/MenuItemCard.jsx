ccaff9d555249f8296e7bfa8b9b43fd8fc5921a4
cpark <cpark931@gmail.com>
2025-03-31 19:34:08 -0700

    added menu item folder and menu item card and owner

---
+++ react-vite/src/components/MenuItem/MenuItemCard/MenuItemCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './MenuItemCard.css';
+
+export default function MenuItemCard({ menuItem }) {
+    return (
+        <>
+        <Link to={'/menu-items/${menuItem.id}'}>
+        <div className='menu-item-card'>
+        <div>
+            ###add image here
+        </div>
+        <div className='menu-item-name'>
+            {menuItem.name}
+        </div>
+        <div className='menu-item-price'>
+            ${menuItem.price}
+        </div>
+        </div>
+        </Link>
+        </>
+    )
+}
---
+++ react-vite/src/components/MenuItem/MenuItemCardOwner/MenuItemCardOwner.jsx
@@ -0,0 +1,59 @@
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import OpenModalButton from '../../components/OpenModalButton';
+import MenuItemDeleteModal from '../MenuItemDeleteModal';
+import MenuItemFormUpdate from '../MenuItemFormUpdate';
+import './MenuItemCardOwner.css';
+
+export default function MenuItemCardOwner({ menuItem }) {
+    const dispatch = useDispatch();
+    const menuItemId = menuItem.id;
+    const restaurantId = menuItem.restaurantId;
+    const [isLoaded, setIsLoaded] = useStatefalse();
+
+    useEffect(() => {
+        setIsLoaded(true);
+    }, [dispatch])
+
+    return (
+        <>
+            {isLoaded && (
+                <div id="menu-item-owner-card">
+                    <div>
+                        ###add image of menu-item-owner-card
+                    </div>
+                    <div id="menu-item-ownder-info">
+                        <span id="menu-item-ownder-info-col-1">
+                            <div id="menu-item-owner-name">
+                                {menuItem.name}
+                            </div>
+                            <div id="menu-item-owner-price">
+                                ${menuItem.price}
+                            </div>
+                        </span>
+                        <span id="menu-item-owner-info-col-2">
+                            <span className="menu-item-form-update-and-delete-modal-btns">
+                                <OpenModalButton
+                                    buttonText="Update"
+                                    modalComponent={
+                                        <MenuItemFormUpdate
+                                            menuItem={menuItem}
+                                        />}
+                                />
+                            </span>
+                            <span className="menu-item-form-update-and-delete-modal-btns">
+                                <OpenModalButton
+                                    buttonText="Delete"
+                                    modalComponent={<MenuItemDeleteModal
+                                        menuItemId={menuItemId}
+                                        restaurantId={restaurantId}
+                                    />}
+                                />
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
