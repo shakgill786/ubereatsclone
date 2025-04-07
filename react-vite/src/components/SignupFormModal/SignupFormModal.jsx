0f228636f6c5f3bebc2cc294eddd2500c08c6b76
shakgill786 <sgillani1@gmail.com>
2025-04-07 11:09:37 -0400

    morefixes

--- react-vite/src/components/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/SignupFormModal/SignupFormModal.jsx
@@ -6,20 +6,23 @@ import "./SignupForm.css";
 
 function SignupFormModal() {
   const dispatch = useDispatch();
+  const { closeModal } = useModal();
+
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
   const [errors, setErrors] = useState({});
-  const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
       return setErrors({
-        confirmPassword:
-          "Confirm Password field must be the same as the Password field",
+        confirmPassword: "Confirm Password must match Password",
       });
     }
 
@@ -28,6 +31,9 @@ function SignupFormModal() {
         email,
         username,
         password,
+        first_name: firstName,
+        last_name: lastName,
+        street_address: streetAddress,
       })
     );
 
@@ -41,48 +47,49 @@ function SignupFormModal() {
   return (
     <>
       <h1>Sign Up</h1>
-      {errors.server && <p>{errors.server}</p>}
       <form onSubmit={handleSubmit}>
         <label>
+          First Name
+          <input value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+        </label>
+        {errors.first_name && <p>{errors.first_name}</p>}
+
+        <label>
+          Last Name
+          <input value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+        </label>
+        {errors.last_name && <p>{errors.last_name}</p>}
+
+        <label>
+          Street Address
+          <input value={streetAddress} onChange={(e) => setStreetAddress(e.target.value)} required />
+        </label>
+        {errors.street_address && <p>{errors.street_address}</p>}
+
+        <label>
           Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <input value={email} onChange={(e) => setEmail(e.target.value)} required />
         </label>
         {errors.email && <p>{errors.email}</p>}
+
         <label>
           Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+          <input value={username} onChange={(e) => setUsername(e.target.value)} required />
         </label>
         {errors.username && <p>{errors.username}</p>}
+
         <label>
           Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </label>
         {errors.password && <p>{errors.password}</p>}
+
         <label>
           Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
+          <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
         </label>
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+
         <button type="submit">Sign Up</button>
       </form>
     </>
